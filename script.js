816cef00866ac62557dba9bbd0b843b665162fd4
karima-st <karimastomma@gmail.com>
2025-05-11 19:13:31 +0000

    new videos added

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const videoPlayer = document.getElementById('videoPlayer');
 // List of video file paths
 const playlist = [
   'videos/video1.mp4',
-  'videos/video4.mp4',
+  //'videos/video2.mp4',
   'videos/video3.mp4',
-  'videos/video2.mp4',
-  'videos/video5.mp4'
+  'videos/video4.mp4',
+  'videos/video5.mp4',
+  'videos/video6.mp4',
+  'videos/video7.mp4',
+  //'videos/video8.mp4',
+  //'videos/video9.mp4'
 ];
 
 let currentIndex = 0;
